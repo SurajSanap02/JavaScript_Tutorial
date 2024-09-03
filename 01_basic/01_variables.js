4da9a8da97cde2d380fc9b89e094406246b13d38
SurajSanap02 <surajsanap9326@gmail.com>
2024-09-03 12:05:21 +0000

    contianer config done

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,13 @@
+const accountId= 1443
+let accountEmail= "suraj@gmail.com"
+var accountPassword= "1234"
+accountCity= "Bombay"
+let accountState;
+
+//acountId =2 //not allowed 
+
+accountEmail = "su@.com"
+accountPassword="3344"
+accountCity="nashik"
+
+console.table =([accountId,accountEmail,accountPassword,accountCity,accountState])
