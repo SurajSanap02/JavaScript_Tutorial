53039b62783062caecd100e18a1476c78745e927
SurajSanap02 <surajsanap9326@gmail.com>
2024-09-03 14:45:19 +0000

    container config done

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict";  //treat all JS Code AS newer Version
+
+// alert (3+3)//we are using nodeJS , not browser 
+
+console.log(3+3);  //code readbility shoubld be high
+
+console.log("suraj");
+
+let name =" Suraj"
+let age = '20'
+let isloggedIn = false
+
+// ***data types***
+// number => (2power(^)53-1) TO -(2^53-1)
+// bigint => big number use for bigint data type (ex.trading web , social media )
+// string => type to double quotes (" ")
+//boolean =>true / false 
+// null   =>standalone value = type (null)
+//undefined => value not definded
+//symbol => unique 
+
+
+// object 
+
+console.log(typeof undefined);//undefined 
+console.log( typeof null);
+
+
