833a04b83fdb99a29e3709f6cf054c78a72ef4ee
SurajSanap02 <surajsanap9326@gmail.com>
2024-09-09 11:13:58 +0000

    conversion of data types

--- 01_basic/03_conversion.js
+++ 01_basic/03_conversion.js
@@ -1,18 +1,33 @@
- let score = null
+ let score = "suraj"
 
 //console.log(typeof score);
-//console.log(typeof (score));
+//console.log(typeof (score));  //method
 
-let valueInnumber = number (score)
+let valueInNumber = Number (score)
 //console.log(typeof valueInNumber);
-//console.log(valueInnumber);
+//console.log(valueInNumber);
 
 
 //"33"=> 33
-//"33avc"=>NAN (not a number)
+//"33avc"=>NAN (not and number)
 // true => 1; false=> 0
 
-let isLoggedIn = 1
+let isLoggedIn = "SURAJ"
 let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false;
+// "" => false;
+// "SURAJ" => true
+
+let  someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
+
 
 
