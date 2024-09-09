f969dfa29eb27281181b9d41f29654db885be8c1
SurajSanap02 <surajsanap9326@gmail.com>
2024-09-09 14:58:35 +0530

    03_conversion.js

--- 01_basic/03_conversion.js
+++ 01_basic/03_conversion.js
@@ -1,11 +1,18 @@
-let score = 33 
+let score = "Suraj"
 
-console.log(typeof score);
-console.log(typeof (score));
+//console.log(typeof score);
+//console.log(typeof (score));
 
 let valueInnumber = number (score)
-console.log(typeof valueInNumber);
-console.log(valueInnumber);
+//console.log(typeof valueInNumber);
+//console.log(valueInnumber);
 
+
+//"33"=> 33
+//"33avc"=>NAN (not a number)
+// true => 1; false=> 0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 
