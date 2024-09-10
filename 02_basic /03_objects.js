fd8abbf63b70a3169101bf6c2951462d4a715ae6
SurajSanap02 <surajsanap9326@gmail.com>
2024-09-10 14:59:09 +0000

    container config done

---
+++ 02_basic /03_objects.js
@@ -0,0 +1,24 @@
+//#objects defind keys & value 
+
+//singleton
+//object.create
+
+//object literals 
+
+const mySym = Symbol("key1")
+
+const jsUser= {
+    name:"SURAJ",
+    [mySym]:"mykey1",
+    age:18,
+    location:"MUMBAI",
+    email:"suraj@gmail.com",
+    isLoggedIn : false,
+    isLoggedDays:["Monday","Sunday"]
+}
+
+console.log(jsUser.email)
+console.log(jsUser["email"])
+console.log(jsUser.name)
+console.log (jsUser[mySym])
+
