d0bbfaa53857c9697d77a1d84f000548afe9ad5f
SurajSanap02 <surajsanap9326@gmail.com>
2024-09-10 11:38:37 +0000

    array

---
+++ 01_basic/07_dates.js
@@ -0,0 +1,17 @@
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(typeof myDate);
+
+//let myCreatedDate = new Date (2023,0,23)//date print metod
+//let myCreatedDate= new Date(2024,0,23,5,3)
+//let myCreatedDate= new Date("2024-01-14")
+let myCreatedDate= new Date("01-14-2024")
+
+console.log(myCreatedDate.toDateString());
+
+let newDate = new Date ()
+console.log(newDate);
+console.log(newDate.getDay);
+
---
+++ 02_basic /01_arrays.js
@@ -0,0 +1,13 @@
+//array 
+
+const myArr = [0,2,3,4,5,6,7,8,9]
+const myCricketrs=["Rohit","virat","Sachin"]
+
+const myArr2 = new Array (1,2,3,4,5)
+console.log(myArr[1]);
+
+//array method 
+
+myArr.push(6) //value add the array 
+console.log(myArr);
+
