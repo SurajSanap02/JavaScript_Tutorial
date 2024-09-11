a11d923ac5d30a3c4807a4b22c840f1476e494e1
SurajSanap02 <surajsanap9326@gmail.com>
2024-09-11 14:26:20 +0000

    function

---
+++ 03.basic/01_function.js
@@ -0,0 +1,43 @@
+
+
+function sayMyName() {
+
+    console.log("S")
+    console.log("U")
+    console.log("R")
+    console.log("A")
+    console.log("J")
+}
+
+function addTwoNumbers(number1, number2) {
+    console.log(number1 + number2);
+    console.log("suraj");
+    //second type of return the output 
+    return number1 + number2
+    return result
+
+}
+
+const result = addTwoNumbers(3, 5)
+console.log("result:", result);
+
+// function using username called 
+function loginUserMessage(username) {
+    return `$ {username} just logged in in`
+}
+console.log(loginUserMessage("suraj"));
+
+
+// function using  login user message  called 
+function loginUserMessage  (username){
+    return `${username} just logged in `
+}
+console.log(loginUserMessage("suraj"));
+
+//
+function calculateCartPrice (num1){  
+ return num1
+}
+ console.log(calculateCartPrice(2));
+  
+
