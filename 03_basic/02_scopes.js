12284a1e0d9ecd064a5f189157825430a2a67b13
SurajSanap02 <surajsanap9326@gmail.com>
2024-09-12 10:35:17 +0000

    scope mini hoisting js

---
+++ 03_basic/02_scopes.js
@@ -0,0 +1,45 @@
+
+// if (true) {
+//     let a = 10
+//     const b = 20
+//     var c = 30
+// }
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// function one() {
+//     const username = "SURAJ"
+
+//     function two() {
+//         const website = "youtube"
+//         console.log(username);
+//     }
+//     console.log(website);
+//     two()
+// }
+// //*****condation*****//
+
+// if (true) {
+//     const username = "suraj"
+//     if (username === "suraj") {
+//         const website = "youtube"
+//         console.log(username + website);
+//     }
+    
+// }
+
+//*************intersting *************//
+
+function adddone(num){
+    return num + 1
+}
+adddone(5)
+
+
+const addTwo = function (num){
+    return num + 2 
+}
+addTwo(2)
