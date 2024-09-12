392848b277160ae808a3998f4ff87a856b468bcb
SurajSanap02 <surajsanap9326@gmail.com>
2024-09-12 11:59:56 +0000

    IIFE

--- 03_basic/03_arrow.js
+++ 03_basic/03_arrow.js
@@ -3,9 +3,29 @@ const user = {
     price:999,
 
     welcomeMessage : function () {
-        console.log(`${this.username},welcome to website`);
+        console.log(`${this.username} , welcome to website`);
+        console.log(this);
         
     }
 }
 user.welcomeMessage
+user.username="sam"
+user.welcomeMessage()
+
+function chai (){
+    console.log(this);
+}
+//*********arrow function*********//
+//syntax
+//  const addTwo = (num1,num2)=>{
+//  return num1 + num2
+//  }
+//*************************************************
+
+
+const one =  ()=>{
+    let username = "suraj"
+    console.log(this.username);
+}
+one()
 
---
+++ 03_basic/04_iife.js
@@ -0,0 +1,7 @@
+// Immediately invoked function Expression (IIFE)
+
+
+function chai(){
+    console.log('DB CONNECTED');
+}
+chai()
